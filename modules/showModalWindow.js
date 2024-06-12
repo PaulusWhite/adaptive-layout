2be21946780dc145188dab9e39019abea3ef9b9a
Pavel <exactlypaulus@gmail.com>
2024-06-12 16:43:31 +0300

    feat: add possibility to display modal window

--- modules/showModalWindow.js
+++ modules/showModalWindow.js
@@ -1,8 +1,14 @@
-// import ModalWindow from "../components/ModalWindow.mjs";
+import ModalWindow from "../components/ModalWindow.mjs";
 
+document.body.addEventListener("click", (e) => {
+  const target = e.target;
 
-// // const btn = document.querySelector(".try-btn");
-// document.body.innerHTML += ModalWindow("signIn")
-// // btn.addEventListener("click", () => {
-// //   document.body.innerHTML += ModalWindow("signIn")
-// // })
+  if(target.closest(".try-btn")){
+    document.body.innerHTML += ModalWindow("signIn")
+  }
+
+  if(target.closest(".modal__options__close-btn")){
+    const modal = document.querySelector(".modal");
+    modal.remove();
+  }
+});
